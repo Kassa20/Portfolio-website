be0c34e02758a633638bdef796bf254b72e933d1
neo <ykassa47@gmail.com>
2023-12-25 21:15:47 -0600

    figure out email problem

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+function sendEmail()
+{
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "ykassa47@gmail.com",
+        Password : "password",
+        To : 'ykassa47@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
